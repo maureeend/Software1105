2164af363fc9866e24c177e80aa19e6a62814578
maureeend <151353727+maureeend@users.noreply.github.com>
2024-05-28 15:13:55 +0300

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -6,59 +6,47 @@ from tkinter import messagebox
 class TestGame(unittest.TestCase):
     def test_game_initialization(self):
         game = Game()
-        # Assert that the game initializes without errors
         self.assertIsInstance(game, Game)
 
     def test_player_color_selection(self):
-        # Test player color selection by confirming chosen colors
         game = Game()
 
         self.assertEqual(game.players[0].color, 'red')
-        # Assuming player 2's color is automatically assigned as yellow
         self.assertEqual(game.players[1].color, 'yellow')
 
     def test_valid_column_selection(self):
-        # Test placing disk in a valid and existing column
         game = Game()
 
         game.drop_disc(3)
-        # Check if the disc is placed in the correct position
         self.assertEqual(game.board.board[5][3], 1)
 
     def test_invalid_column_selection(self):
-        # Test placing disk in an invalid and non-existing column
         game = Game()
-        # Attempt to place disk in column 8 (non-existing)
         with self.assertRaises(IndexError):
             game.drop_disc(8)
 
     def test_disk_placement_empty_column(self):
-        # Test placing disk in an empty column
         game = Game()
         game.drop_disc(3)
-        # Check if the disc is placed in the correct position
         self.assertEqual(game.board.board[5][3], 1)
 
     def test_disk_placement_solid_column(self):
         game = Game()
         
-        # Fill up column 3
         for _ in range(6):
             game.drop_disc(3)
         
-        # Test placing a disc in a full column
         with patch.object(messagebox, 'showerror') as mock_showerror:
             game.drop_disc(3)
             mock_showerror.assert_called_once_with("Invalid Move", "This column is full. Please choose another one.")
 
     def test_victory_condition_vertical_alignment(self):
-        # Test victory condition with vertical alignment of 4 discs
         game = Game()
         game.board.board[0][3] = 1
         game.board.board[1][3] = 1
         game.board.board[2][3] = 1
         game.board.board[3][3] = 1
-        # Check if the game declares the first player as the winner
+
         self.assertTrue(game.check_win(1))
 
 
